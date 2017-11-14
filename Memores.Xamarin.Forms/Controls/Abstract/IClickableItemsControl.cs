71e2fc0b325ec36618def2e06419622eba1ee2f6
Павел <pdorovsky@gmail.com>
2017-11-14 20:25:13 +0300

    ClickableListView impl for Xamarin.Forms

---
+++ Memores.Xamarin.Forms/Controls/Abstract/IClickableItemsControl.cs
@@ -0,0 +1,7 @@
+﻿using System.Windows.Input;
+
+namespace Memores.Xamarin.Forms.Controls.Abstract {
+    public interface IClickableItemsControl {
+        ICommand ItemClickCommand { get; set; }
+    }
+}
---
+++ Memores.Xamarin.Forms/Controls/ClickableListView.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+using Memores.Xamarin.Forms.Controls.Abstract;
+
+namespace Memores.Xamarin.Forms.Controls {
+    public class ClickableListView : ListView, IClickableItemsControl {
+        public static BindableProperty ItemClickCommandProperty = BindableProperty.Create<ClickableListView, ICommand>(x => x.ItemClickCommand, null);
+
+        public ICommand ItemClickCommand {
+            get { return (ICommand) GetValue(ItemClickCommandProperty); }
+            set { SetValue(ItemClickCommandProperty, value); }
+        }
+
+
+        public ClickableListView() {
+            ItemTapped += OnItemTapped;
+        }
+
+
+        void OnItemTapped(object sender, ItemTappedEventArgs args) {
+            if (args.Item != null && ItemClickCommand != null && ItemClickCommand.CanExecute(args)) {
+                ItemClickCommand.Execute(args.Item);
+                SelectedItem = null;
+            }
+        }
+    }
+}
